8bd6094fe4d5f294ef040b11888a60046332be59
leo <wupengcn301@gmail.com>
2017-12-16 15:20:31 +0800

    修复格式问题

--- src/learn_english/run.sh
+++ src/learn_english/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./captain_env/bin/python2.7 src/learn_english/model/retrieve.py
-nohup ./captain_env/bin/python2.7 src/learn_english/model/db_sync.py &
+./captain_env/bin/python2.7 src/learn_english/model/db_sync.py
 nohup ./captain_env/bin/python2.7 src/learn_english/model/tackle_word.py &
 nohup ./captain_env/bin/python2.7 src/learn_english/model/captain_hub.py &
 nohup ./captain_env/bin/python2.7 src/learn_english/model/vocabulary_list.py &
